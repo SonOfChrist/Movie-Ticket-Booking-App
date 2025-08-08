b012e653e0fb22854217e63bf6a2131527577bf2
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-08-08 12:07:01 +0300

    Changed-Updated env file

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Load environment variables from .env file and set them in process.env so they can be accessed in the application
 const connectDB = async () => {
   try {
     mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'));
