9c6ee40a9632f6582e651e5f1a553d579dc1f621
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-09-03 08:56:32 +0300

    Update Server Side

--- Server/server.js
+++ Server/server.js
@@ -17,6 +17,6 @@ app.use(cors());
 app.use(clerkMiddleware())
 
 // Routes
-app.get('/', (req, res) => res.send('Welcome to the Movie Ticket App Server!'));
+app.get('/', (req, res) => res.send('Welcome to the Movie Ticket App Server is Live!'));
 app.use('/api/inngest', serve({ client: inngest, functions }));
 app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`);});
