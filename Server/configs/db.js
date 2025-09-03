62b7e96669e32e3b256cda1daf47be0c1de30c61
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-09-03 10:10:35 +0300

    Created-Updated Serve

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 // Load environment variables from .env file and set them in process.env so they can be accessed in the application
 const connectDB = async () => {
   try {
-    mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'));
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI }/quickshow`);
+    mongoose.connection.on('connected', () => console.log('Database connected successfully'));
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
- 
   }
 } 
 export default connectDB;  
--- Server/server.js
+++ Server/server.js
@@ -18,5 +18,6 @@ app.use(clerkMiddleware())
 
 // Routes
 app.get('/', (req, res) => res.send('Welcome to the Movie Ticket App Server is Live!'));
-app.use('/api/inngest', serve({ client: inngest, functions }));
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`);});
